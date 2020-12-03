bfbd924d0da7daecdcf54805fa5bedbef67e7858
Liarr <liarr@foxmail.com>
2020-12-03 23:53:16 +0800

    Remove Module Dependencies

--- library/src/main/java/com/gohsdk/utils/LogUtil.java
+++ library/src/main/java/com/gohsdk/utils/LogUtil.java
@@ -1,4 +1,67 @@
 package com.gohsdk.utils;
 
+import android.util.Log;
+
 public class LogUtil {
+
+    private static final int VERBOSE = 1;
+    private static final int DEBUG = 2;
+    private static final int INFO = 3;
+    private static final int WARN = 4;
+    private static final int ERROR = 5;
+    private static final int NOTHING = 6;
+
+    private static final String TAG = "GohSDK";
+
+    private static int level = VERBOSE;
+
+    public static void v(String msg) {
+        v(TAG, msg);
+    }
+
+    public static void v(String tag, String msg) {
+        if (level <= VERBOSE) {
+            Log.v(tag, msg);
+        }
+    }
+
+    public static void d(String msg) {
+        d(TAG, msg);
+    }
+
+    public static void d(String tag, String msg) {
+        if (level <= DEBUG) {
+            Log.d(tag, msg);
+        }
+    }
+
+    public static void i(String msg) {
+        i(TAG, msg);
+    }
+
+    public static void i(String tag, String msg) {
+        if (level <= INFO) {
+            Log.i(tag, msg);
+        }
+    }
+
+    public static void w(String msg) {
+        w(TAG, msg);
+    }
+
+    public static void w(String tag, String msg) {
+        if (level <= WARN) {
+            Log.w(tag, msg);
+        }
+    }
+
+    public static void e(String msg) {
+        e(TAG, msg);
+    }
+
+    public static void e(String tag, String msg) {
+        if (level <= ERROR) {
+            Log.e(tag, msg);
+        }
+    }
 }
