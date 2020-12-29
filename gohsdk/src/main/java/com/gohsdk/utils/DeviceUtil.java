570c10979bb922a252e761d56e09742789d7aa8e
Liarr <40908569+LiarrDev@users.noreply.github.com>
2020-12-29 14:56:42 +0800

    Update DeviceUtil.java

--- gohsdk/src/main/java/com/gohsdk/utils/DeviceUtil.java
+++ gohsdk/src/main/java/com/gohsdk/utils/DeviceUtil.java
@@ -2,6 +2,7 @@ package com.gohsdk.utils;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.provider.Settings;
 
 public class DeviceUtil {
 
@@ -10,5 +11,40 @@ public class DeviceUtil {
      */
     public static boolean isPortrait(Context context) {
         return context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+    }
+    
+    /**
+     * 屏幕宽度
+     */
+    public static int getScreenWidth(Context context) {
+        return context.getResources().getDisplayMetrics().widthPixels;
+    }
+
+    /**
+     * 屏幕高度
+     */
+    public static int getScreenHeight(Context context) {
+        return context.getResources().getDisplayMetrics().heightPixels;
+    }
+
+    /**
+     * 屏幕尺寸
+     */
+    public static String getScreenSize(Context context) {
+        return getScreenHeight(context) + "×" + getScreenWidth(context);
+    }
+
+    /**
+     * 设备型号
+     */
+    public static String getPhoneModel() {
+        return android.os.Build.MANUFACTURER + " " + android.os.Build.MODEL;
+    }
+
+    /**
+     * ANDROID_ID
+     */
+    public static String getAndroidId(Context context) {
+        return Settings.System.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
     }
 }
